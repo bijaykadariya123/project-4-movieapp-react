15c434d9a086b507c8e64e770a509862b8b993ab
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-09-29 22:11:01 -0400

    cSS

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,41 +6,43 @@ function Index (props){
     const Movies = useLoaderData()
     return <>
     <div className='indexPage'>
-            <div>
-                <h3>Feature Movies</h3>
-                <div id="carouselExample" class="carousel slide">
-                    <div class="carousel-inner" >
-                        <div class="carousel-item active">
-                            <img src="https://m.media-amazon.com/images/M/MV5BMTU2OTYzODQyMF5BMl5BanBnXkFtZTgwNjU3Njk5NTM@._V1_.jpg" class="d-block w-100" alt="..."/>
+        <div className="hide">  
+                <div className="leftBarIndex">
+                    <h3 className="titleFeaturMovies">Most Viewed</h3>
+                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner" >
+                            <div class="carousel-item active">
+                                <img  src="https://m.media-amazon.com/images/M/MV5BMTU2OTYzODQyMF5BMl5BanBnXkFtZTgwNjU3Njk5NTM@._V1_.jpg" class="d-block w-100" alt="img"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img  src="https://www.hollywoodreporter.com/wp-content/uploads/2023/07/rev-1-BAR-07462_High_Res_JPEG-H-2023.jpg?w=1296" class="d-block w-100" alt="img"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img  src="https://i.pinimg.com/originals/d3/3c/4e/d33c4ef837ce642649d80bd0b9bbddd7.jpg" class="d-block w-100" alt="img"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img  src="https://thumbnails.cbsig.net/CBS_Production_Entertainment_VMS/2023/05/18/2208127043535/MIDR1_US_2023_SA_16x9_1920x1080_NB_2215190_1920x1080.jpg" class="d-block w-100" alt="img"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img  src="https://npr.brightspotcdn.com/17/68/0e67a5244c879cdedda91045ff89/wick-2.jpg" class="d-block w-100" alt="img"/>
+                            </div>
+                            <div class="carousel-item">
+                                <img  src="https://images.thedirect.com/media/article_full/when-will-fast-furious-10-start-streaming.jpg" class="d-block w-100" alt="img"/>
+                            </div>
                         </div>
-                        <div class="carousel-item">
-                            <img src="https://www.hollywoodreporter.com/wp-content/uploads/2023/07/rev-1-BAR-07462_High_Res_JPEG-H-2023.jpg?w=1296" class="d-block w-100" alt="..."/>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="https://i.pinimg.com/originals/d3/3c/4e/d33c4ef837ce642649d80bd0b9bbddd7.jpg" class="d-block w-100" alt="..."/>
-                        </div>
-                        <div class="carousel-item">
-                        <img src="https://thumbnails.cbsig.net/CBS_Production_Entertainment_VMS/2023/05/18/2208127043535/MIDR1_US_2023_SA_16x9_1920x1080_NB_2215190_1920x1080.jpg" class="d-block w-100" alt="..."/>
-                        </div>
-                        <div class="carousel-item">
-                        <img src="https://npr.brightspotcdn.com/17/68/0e67a5244c879cdedda91045ff89/wick-2.jpg" class="d-block w-100" alt="..."/>
-                        </div>
-                        <div class="carousel-item">
-                        <img src="https://images.thedirect.com/media/article_full/when-will-fast-furious-10-start-streaming.jpg" class="d-block w-100" alt="..."/>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>          
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>    
+                </div>      
 
      
-                <div>
+                <div className="createForm">
                     <h3>Create Movie</h3>
                     <Form method="POST" action="/create">
                         <input class="form-control" type  = "text" name="name" placeholder="Movie Name"/>
@@ -49,7 +51,7 @@ function Index (props){
                         <input class="form-control" type = "text" name="image" placeholder="Image"/>
                         <input class="form-control" type = "text" name="director" placeholder="Director"/>
                         <input class="form-control" type = "text" name="country" placeholder="Country"/>
-                        <button>Add Movie</button>
+                        <button class="btn btn-secondary"><span>Add Movie</span></button>
                     </Form>
                 </div>
             </div>
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -4,30 +4,46 @@ function Show (props){
     const movie = useLoaderData()
 
     return(
-        <div>
-            <img src={movie.image} alt ={movie.name}/>
-            <h3>{movie.name}</h3>
-            <h4>{movie.genre}</h4>
-            <h4>{movie.year}</h4>
-            <h4>{movie.director}</h4>
-            <h4>{movie.country}</h4>
+        <div className='showPage'>
+             <Link className='homeLink' to='/'>Home</Link>
             <div>
-                <h2>Update Movie</h2>
-                    <Form method="POST" action={`/update/${movie.id}`}>
-                    <input type = "text" name="name" placeholder="Movie Name" defaultValue={movie.name}/>
-                    <input type = "text" name="genre" placeholder="Genre" defaultValue={movie.genre}/>
-                    <input type = "text" name="year" placeholder="Year" defaultValue={movie.year}/>
-                    <input type = "text" name="image" placeholder="Image" defaultValue={movie.image}/>
-                    <input type = "text" name="director" placeholder="Director" defaultValue={movie.director}/>
-                    <input type = "text" name="country" placeholder="Country" defaultValue={movie.country}/>
-                    <button>Update Movie</button>
-                </Form>
-
-                <Form method='post' action={`/delete/${movie.id}`}>
-                <button>Delete</button>
-                </Form>
+                <div className='showPicDescription'>
+                    <div className='showPic'>
+                        <img src={movie.image} alt ={movie.name}/>
+                    </div>
+                    <div className='showDescription'>
+                        <h2 className="description">Movie Name:{movie.name}</h2>
+                        <h4 className="description">Genre: {movie.genre}</h4>
+                        <h4 className="description">Make: {movie.year}</h4>
+                        <h4 className="description">Directed By:{movie.director}</h4>
+                        <h4 className="description">Country: {movie.country}</h4>
+                    </div>
                 </div>
-            <Link to='/'>Back to Index</Link>
+                <div className='updateMovie'>
+                    <div className='updateMovie-sub'>
+                        <h2>Update Movie</h2>
+                        <div className='updatede'>
+                            <div>
+                                <Form method="POST" action={`/update/${movie.id}`}>
+                                    <input class="form-control"  type = "text" name="name" placeholder="Movie Name" defaultValue={movie.name}/>
+                                    <input class="form-control"  type = "text" name="genre" placeholder="Genre" defaultValue={movie.genre}/>
+                                    <input class="form-control"  type = "text" name="year" placeholder="Year" defaultValue={movie.year}/>
+                                    <input class="form-control"  type = "text" name="image" placeholder="Image" defaultValue={movie.image}/>
+                                    <input class="form-control"  type = "text" name="director" placeholder="Director" defaultValue={movie.director}/>
+                                    <input class="form-control"  type = "text" name="country" placeholder="Country" defaultValue={movie.country}/>
+                                    <button class="btn btn-secondary"><span className="updatedeleteButton up-mar">Update Movie</span></button>
+                                </Form>
+                            </div>
+                            <div>
+                                <Form method='post' action={`/delete/${movie.id}`}>
+                                <button class="btn btn-secondary"> <span className="updatedeleteButton">Delete Movie</span></button>
+                                </Form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
         </div>
     )
 
